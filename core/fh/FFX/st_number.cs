7a2ad8cadfd65005a593df0a38899d69adbbb9ea
cxldalyy <callum.daly2504@gmail.com>
2026-03-01 14:37:16 +0000

    Label StNumber Fields
    StNumber is responsible for displaying character commands in menus - most notably overdrives in the party menu. Each character takes their ID, type, and then command associated. 
    E.G:
    00 00 60 30 -> Tidus, Overdrive, Spiral Cut. 
    00 48 19 31 -> Tidus, Overdrive Category, Swordplay.
    Aeons are also included - with 1 being "Special Attack" and 2 being "Overdrive" for them.

--- core/fh/FFX/st_number.cs
+++ core/fh/FFX/st_number.cs
@@ -3,9 +3,7 @@ namespace Fahrenheit.FFX;
 // This is likely to get folded into a different file once we figure out what it's for
 [StructLayout(LayoutKind.Explicit, Size = 0x4)]
 public struct StNumber {
-    [FieldOffset(0x0)] public byte __0x0;
-    [FieldOffset(0x1)] public byte __0x1;
-
-    // Can be a command id or some other unknown id if __0x0 == 1
-    [FieldOffset(0x2)] public short __0x2;
+    [FieldOffset(0x0)] public byte  category;     // Can be a character id or menu
+    [FieldOffset(0x1)] public byte  type; 
+    [FieldOffset(0x2)] public short command_name; // Can be a Command id or Aeon id if category is 0x1 (Yuna)
 }

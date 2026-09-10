04ba41dd343fa9dbf5c5b8256b9315818531dff1
Dawn Veilwinter <DawnVeilwinter@protonmail.com>
2026-09-10 19:46:51 +0200

    Revert making the FadeHelper fields public

--- src/core/gui/fade.cs
+++ src/core/gui/fade.cs
@@ -7,8 +7,8 @@ namespace Fahrenheit.Gui;
 
 /// <summary>Provides utilities to help with fading from one color to another.</summary>
 public class FadeHelper : Timer {
-    public uint color_from { get; protected set; }
-    public uint color_to   { get; protected set; }
+    protected uint color_from;
+    protected uint color_to;
 
     /// <summary>
     ///     Create a new FadeHelper to fade from and to specified colors over a given amount of time.

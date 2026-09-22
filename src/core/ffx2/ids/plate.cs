0f7eefbec7b97ffd59eff70f9769f9372aa89c7a
cxldalyy <callum.daly2504@gmail.com>
2026-09-22 20:10:48 +0000

    Update a handful of X-2's ids

---
+++ src/core/ffx2/ids/plate.cs
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+// This file is part of Fahrenheit, © 2023-2026 The Fahrenheit contributors.
+// It is licensed to you under the GNU Lesser General Public License, version 3.0 or later. See COPYING, COPYING.LESSER.
+
+// ffx_ps2/ffx2/master/jppc/battle/kernel/plate.h
+// Switch release of FFX/X-2 HD
+
+namespace Fahrenheit.FFX2.Ids;
+
+public static class PlateId {
+    public const T_X2PlateId PLATE_FIRST_STEPS         = 0x6000;
+    public const T_X2PlateId PLATE_VANGUARD            = 0x6001;
+    public const T_X2PlateId PLATE_BUM_RUSH            = 0x6002;
+    public const T_X2PlateId PLATE_UNDYING_STORM       = 0x6003;
+    public const T_X2PlateId PLATE_FLASH_OF_STEEL      = 0x6004;
+    public const T_X2PlateId PLATE_PROTECTION_HALO     = 0x6005;
+    public const T_X2PlateId PLATE_HOUR_OF_NEED        = 0x6006;
+    public const T_X2PlateId PLATE_UNWAVERING_GUARD    = 0x6007;
+    public const T_X2PlateId PLATE_VALIANT_LUSTRE      = 0x6008;
+    public const T_X2PlateId PLATE_HIGHROAD_WINDS      = 0x6009;
+    public const T_X2PlateId PLATE_MOUNTED_ASSAULT     = 0x600A;
+    public const T_X2PlateId PLATE_HEART_OF_FLAME      = 0x600B;
+    public const T_X2PlateId PLATE_ICE_QUEEN           = 0x600C;
+    public const T_X2PlateId PLATE_THUNDER_SPAWN       = 0x600D;
+    public const T_X2PlateId PLATE_MENACE_OF_THE_DEEP  = 0x600E;
+    public const T_X2PlateId PLATE_DOWNTRODDER         = 0x600F;
+    public const T_X2PlateId PLATE_SACRED_BEAST        = 0x6010;
+    public const T_X2PlateId PLATE_TETRA_MASTER        = 0x6011;
+    public const T_X2PlateId PLATE_RESTLESS_SLEEP      = 0x6012;
+    public const T_X2PlateId PLATE_STILL_OF_NIGHT      = 0x6013;
+    public const T_X2PlateId PLATE_MORTAL_COIL         = 0x6014;
+    public const T_X2PlateId PLATE_RAGING_GIANT        = 0x6015;
+    public const T_X2PlateId PLATE_BITTER_FAREWELL     = 0x6016;
+    public const T_X2PlateId PLATE_SELENE_GUARD        = 0x6017;
+    public const T_X2PlateId PLATE_HELIOS_GUARD        = 0x6018;
+    public const T_X2PlateId PLATE_SHINING_MIRROR      = 0x6019;
+    public const T_X2PlateId PLATE_COVETOUS            = 0x601A;
+    public const T_X2PlateId PLATE_DISASTER_IN_BLOOM   = 0x601B;
+    public const T_X2PlateId PLATE_SCOURGEBANE         = 0x601C;
+    public const T_X2PlateId PLATE_HEALING_WIND        = 0x601D;
+    public const T_X2PlateId PLATE_HEART_REBORN        = 0x601E;
+    public const T_X2PlateId PLATE_HEALING_LIGHT       = 0x601F;
+    public const T_X2PlateId PLATE_IMMORTAL_SOUL       = 0x6020;
+    public const T_X2PlateId PLATE_WISH_BRINGER        = 0x6021;
+    public const T_X2PlateId PLATE_STRENGTH_OF_ONE     = 0x6022;
+    public const T_X2PlateId PLATE_SEETHING_CAULDRON   = 0x6023;
+    public const T_X2PlateId PLATE_STONEHEWN           = 0x6024;
+    public const T_X2PlateId PLATE_ENIGMA_PLATE        = 0x6025;
+    public const T_X2PlateId PLATE_HOWLING_WIND        = 0x6026;
+    public const T_X2PlateId PLATE_RAY_OF_HOPE         = 0x6027;
+    public const T_X2PlateId PLATE_PRIDE_OF_THE_SWORD  = 0x6028;
+    public const T_X2PlateId PLATE_SAMURAIS_HONOR      = 0x6029;
+    public const T_X2PlateId PLATE_BLOOD_OF_THE_BEAST  = 0x602A;
+    public const T_X2PlateId PLATE_CHAOS_MAELSTROM     = 0x602B;
+    public const T_X2PlateId PLATE_WHITE_SIGNET        = 0x602C;
+    public const T_X2PlateId PLATE_BLACK_TABARD        = 0x602D;
+    public const T_X2PlateId PLATE_MERCURIAL_STRIKE    = 0x602E;
+    public const T_X2PlateId PLATE_TRICKS_OF_THE_TRADE = 0x602F;
+    public const T_X2PlateId PLATE_HORN_OF_PLENTY      = 0x6030;
+    public const T_X2PlateId PLATE_TREASURE_HUNT       = 0x6031;
+    public const T_X2PlateId PLATE_TEMPERED_WILL       = 0x6032;
+    public const T_X2PlateId PLATE_COVENANT_OF_GROWTH  = 0x6033;
+    public const T_X2PlateId PLATE_SALVATION_PROMISED  = 0x6034;
+    public const T_X2PlateId PLATE_CONFLAGRATION       = 0x6035;
+    public const T_X2PlateId PLATE_SUPREME_LIGHT       = 0x6036;
+    public const T_X2PlateId PLATE_MEGIDDO             = 0x6037;
+    public const T_X2PlateId PLATE_UNERRING_PATH       = 0x6038;
+    public const T_X2PlateId PLATE_FONT_OF_POWER       = 0x6039;
+    public const T_X2PlateId PLATE_HIGHER_POWER        = 0x603A;
+    public const T_X2PlateId PLATE_THE_END             = 0x603B;
+    public const T_X2PlateId PLATE_INTREPID            = 0x603C;
+    public const T_X2PlateId PLATE_ABOMINABLE          = 0x603D;
+    public const T_X2PlateId PLATE_PEERLESS            = 0x603E;
+    public const T_X2PlateId PLATE_LAST_RESORT         = 0x603F;
+}
---
+++ src/core/ffx2/ids/ply_save.cs
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+// This file is part of Fahrenheit, © 2023-2026 The Fahrenheit contributors.
+// It is licensed to you under the GNU Lesser General Public License, version 3.0 or later. See COPYING, COPYING.LESSER.
+
+// ffx_ps2/ffx2/master/jppc/battle/kernel/ply_save.h
+// Switch release of FFX/X-2 HD
+
+namespace Fahrenheit.FFX2.Ids;
+
+public static class PlySaveId {
+    public const T_X2PlySaveId PC_YUNA           = 0x00;
+    public const T_X2PlySaveId PC_RIKKU          = 0x01;
+    public const T_X2PlySaveId PC_PAINE          = 0x02;
+    public const T_X2PlySaveId PC_RIGHT_PISTIL   = 0x03;
+    public const T_X2PlySaveId PC_LEFT_PISTIL    = 0x04;
+    public const T_X2PlySaveId PC_SMASHER_R      = 0x05;
+    public const T_X2PlySaveId PC_CRUSHER_L      = 0x06;
+    public const T_X2PlySaveId PC_DEXTRAL_WING   = 0x07;
+    public const T_X2PlySaveId PC_SINISTRAL_WING = 0x08;
+    public const T_X2PlySaveId PC_KOGORO         = 0x09;
+    public const T_X2PlySaveId PC_GHIKI          = 0x0A;
+    public const T_X2PlySaveId PC_FLURRY         = 0x0B;
+    public const T_X2PlySaveId PC_GULLWINGS      = 0x0C;
+    public const T_X2PlySaveId PC_DUMMY2         = 0x0D;
+    public const T_X2PlySaveId PC_DUMMY3         = 0x0E;
+    public const T_X2PlySaveId PC_CREATURE1      = 0x0F;
+    public const T_X2PlySaveId PC_CREATURE2      = 0x10;
+    public const T_X2PlySaveId PC_CREATURE3      = 0x11;
+    public const T_X2PlySaveId PC_CREATURE4      = 0x12;
+    public const T_X2PlySaveId PC_CREATURE5      = 0x13;
+    public const T_X2PlySaveId PC_CREATURE6      = 0x14;
+    public const T_X2PlySaveId PC_CREATURE7      = 0x15;
+    public const T_X2PlySaveId PC_CREATURE8      = 0x16;
+}

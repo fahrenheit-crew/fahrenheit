d1ce153ee70397c4fcbd082c9d806289edf8a5af
cxldalyy <callum.daly2504@gmail.com>
2026-09-22 20:16:44 +0000

    Update X-2 `typedefs`

--- src/core/ffx2/typedefs.cs
+++ src/core/ffx2/typedefs.cs
@@ -8,13 +8,18 @@
  * of any primitive declaration here, in one spot. You should follow this approach when adding new constants.
  */
 
-global using T_X2CommandId         = System.UInt16; // x2_idpcom
-global using T_X2BtlId             = System.UInt32; // x2_idbtl
-global using T_X2ChrStatId         = System.Int32;  // x2_idchrstat
+global using T_X2CommandId         = System.UInt16; // ids/com_mon, com_ply, item
+global using T_X2TargetId          = System.Int32;  // ids/target
+global using T_X2BtlId             = System.UInt32; // ids/btl
+global using T_X2ChrStatId         = System.Int32;  // ids/chr_stat
 global using T_X2SeTypeId          = System.Byte;   // ids/setype
 global using T_X2BtlSeTypeId       = System.Byte;   // ids/btl_setype
 global using T_X2BtlRequestTagId   = System.Int16;  // ids/btl_req_tags
 global using T_X2BtlRequestActorId = System.Int16;  // ids/btl_req_tags
 global using T_X2BtlVoiceId        = System.UInt32; // ids/btl_voice
 global using T_X2PlySaveId         = System.Int32;  // ids/plysave
-global using T_X2JobId             = System.Int32;  // ids/job
+global using T_X2JobId             = System.UInt16; // ids/job
+global using T_X2PlateId           = System.UInt16; // ids/plate
+global using T_X2AutoAbilityId     = System.UInt16; // ids/a_ability
+global using T_X2AccessoryId       = System.UInt16; // ids/accessory
+global using T_X2KeyItemId         = System.UInt16; // ids/important

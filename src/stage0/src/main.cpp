fd45313847410f79ce4f18eac0b3f81e2eed6903
Fran Kelava <peppy@peppy.hr>
2026-09-26 17:08:29 +0200

    Review

--- src/stage0/src/main.cpp
+++ src/stage0/src/main.cpp
@@ -250,8 +250,8 @@ int __cdecl wmain(
     if (hr != S_OK)
         return hr;
 
-    bool external_debug  = wcsstr(args_self, L"--external-debugger") != nullptr;
-    bool wait_for_attach = wcsstr(args_self, L"--wait-for-attach")   != nullptr;
+    bool external_debug  = wcsstr(args_self, L"--extdbg") != nullptr;
+    bool wait_for_attach = wcsstr(args_self, L"--wait")   != nullptr;
 
     DWORD creation_flags = external_debug
         ? CREATE_SUSPENDED
@@ -279,7 +279,7 @@ int __cdecl wmain(
         return 1;
     }
 
-    // Pause for external debugger attach if `--debug` arg is passed.
+    // Pause for external debugger attach if `--wait` arg is passed.
     if (wait_for_attach) {
         fwprintf_s(stdout, L"You can now attach a debugger; press any key to continue.\n");
         int i = _getch();
